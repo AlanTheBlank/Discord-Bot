e0275c292d1e69b0d556d7978f359e9d86466c64
AlanTheBlank <bigal20001@hotmail.co.uk>
2020-02-02 17:13:10 +0000

    Update discord bot.py

--- discord bot.py
+++ discord bot.py
@@ -3,3 +3,16 @@ import discord
 frim discord.ext import commands
 import asyncio
 
+client = discord.Client(command_prefix=".", activity=discord.CustomActivity("This is basically SSH"))
+
+TOKEN = "
+
+@client.event
+async def on_ready():
+	print("Ready!")
+	
+@client.command(pass_context=True)
+async def run(ctx):
+	await ctx.send(ctx.message[4:])
+	
+client.run(TOKEN)
